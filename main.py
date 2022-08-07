851a157bfa4e9f9b8f15a43f8e1e8867c824827f
stein-sha2197 <95261695+stein-sha2197@users.noreply.github.com>
2022-08-07 16:30:21 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,74 @@
+"""
+Sharon Steinke
+Who's Birthday is it Anyway?
+
+
+"""
+
+class User:
+    """
+    creates user onject with user's name, email, phone number, user name, and password
+    can update info. security email if password is forgotten
+    """
+    pass
+
+contact_book = {}
+
+class Contact:
+    """
+    creates a contact object
+    """
+    contacts = 0
+    def __init__(self, first, last, pronoun, birthday):
+        self.first = first
+        self.last = last
+        self.pronoun = pronoun
+        self.birthday = birthday
+
+        contacts += 1
+    def add_photo(self):
+        """adds a contact photo"""
+        pass
+    def add_phone(self):
+        """add or change phone number"""
+        pass
+    def add_email(self):
+        """add or change email"""
+        pass
+    def add_address(self):
+        """add or change address"""
+        pass
+    def add_previousgift(self):
+        """adds to a list. Can edit list"""
+        pass
+    def add_wishlist(self):
+        """adds to a list. Can edit list"""
+        pass
+    def add_favorite(self):
+        """adds to a list. Can edit list"""
+        pass
+    def add_reminder(self):
+        """adds when user would like to be
+        reminded about this contacts birthday"""
+        pass
+
+def mainmenu(select):
+    """add contact, update wishlist, search contacts"""
+    pass
+
+def send_reminder(contact):
+    """sends reminder of to user based on user
+    preference"""
+    pass
+
+def update_contactbook(contact):
+    """updates global dictionary with contact object"""
+    pass
+
+def main():
+    """runs the program"""
+    pass
+
+
+if __name__ == "__main__":
+    main()
